307855d41ef8151d9f941612360d7bfa445b0ae1
UserTharushi <40-bit-0025@kdu.ac.lk>
2025-08-06 13:17:14 +0530

    Python Project

---
+++ Activity1.py
@@ -0,0 +1,40 @@
+print("Hello, Tharushi!")
+print("Welcome to python")
+print(7+3)
+print(type(6.66))
+print(10%2==0)
+print("AI" + "Chatbot")
+i = 20
+if(i<15):
+    print("i is smaller than 15")  
+    print("i'm in if Block")
+else:
+    print("i is greater than 15")
+    print("i'm in else Block")
+    print("i'm not in if and not in else Block")
+i=20
+if(i==10):
+    print("i is 10")
+elif(i==15):
+    print("i is 15")
+elif(i==20):
+    print("i is 20")
+else:
+    print("i is not present")
+
+i=1
+while i<6:
+    print(i)
+    i += 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+mark1 = float(input("Enter your marks 1: "))
+mark2 = float(input("Enter your marks 2: "))
+total = mark1 + mark2
+print("Total marks:", total)
+
+
+
+
